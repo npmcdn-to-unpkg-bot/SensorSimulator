575bf21722114ad7c611f02cbc4e0123d21f9749
Ben Willis <benjamin.willis@ocado.com>
2016-09-07 16:02:18 +0100

    Fixed conflict?

--- run.py
+++ run.py
@@ -18,34 +18,7 @@ parser.add_argument('-d', metavar="DEVICE ID",
 
 args = parser.parse_args()
 
-<<<<<<< HEAD
-    van.start()
-
-    lats = [lat for ((lat, lon), time, weather, pollution) in van.readings]
-    lons = [lon for ((lat, lon), time, weather, pollution) in van.readings]
-
-    for reading in van.readings:
-        if reading[2] > 27:
-            colour = "#800026"
-        elif reading[2] > 25:
-            colour = "#bd0026"
-        elif reading[2] > 23:
-            colour = "#e31a1c"
-        elif reading[2] > 21:
-            colour = "#fc4e2a"
-        elif reading[2] > 19:
-            colour = "#fd8d3c"
-        elif reading[2] > 17:
-            colour = "#feb24c"
-        elif reading[2] > 15:
-            colour = "#fed976"
-        elif reading[2] > 13:
-            colour = "#ffeda0"
-        else:
-            colour = "#ffffcc"
-=======
 with open(args.route) as route:
     van = VanSimulator(route, args.t, args.speed, args.d)
->>>>>>> develop
 
     van.start()
